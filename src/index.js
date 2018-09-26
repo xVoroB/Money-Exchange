edb4eb4df546b37c2af9cb214cfe4fbdfa834dfa
Vlad Borovsky <xvorob@gmail.com>
2018-09-26 09:25:58 +0300

    CountingCoins

--- src/index.js
+++ src/index.js
@@ -1,5 +1,36 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+  H=0;
+  Q=0;
+  D=0;
+  N=0;
+  P=0;
+
+  if (currency===0) {return {};}
+  else if (currency>10000) {return { error: 'You are rich, my friend! We don\'t have so much coins for exchange' }
+;}
+  for (currency; currency>0; currency--) {
+    P++;
+    if (P===5) {P=0; N++;}
+    if (N===2) {D++; N=0;}
+    if ((D===2) && (N===1)) {D=0; N=0; Q++;}
+    if (Q===2) {H++; Q=0;}
+  }
+
+  const money = {
+    H:H,
+    Q:Q,
+    D:D,
+    N:N,
+    P:P,
+  }
+
+ if (money.H === 0) {delete money.H;}
+ if (money.Q === 0) {delete money.Q;}
+ if (money.D === 0) {delete money.D;}
+ if (money.N === 0) {delete money.N;}
+ if (money.P === 0) {delete money.P;}
+
+  return money;
+
 }
